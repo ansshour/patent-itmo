af124b06a0276492e79359a73e6cba6816c00da5
ansshour <andrey.zhukov001@gmail.com>
2022-12-06 01:41:57 +0300

    add accordions simple search page

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import styles from "./Accordion.module.css"
+import { ReactComponent as Arrow } from "./res/arrow.svg"
+
+export const Accordion = ({ name, children }) => {
+
+    const [open, setOpen] = useState(false)
+
+    return (
+        <div className={styles.container}>
+            <div
+                className={styles.main}
+                onClick={() => { setOpen(!open) }}>
+                <p>{name}</p>
+                <span><Arrow className={open ? [styles.arrow, styles.open].join(" ") : styles.arrow} /></span>
+            </div>
+            <div className={open ? [styles.content, styles.open].join(" ") : styles.content}>
+                <p>hui</p>
+                <p>hui</p>
+                <p>hui</p>
+                <p>hui</p>
+            </div>
+        </div>
+    )
+}
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,6 +1,7 @@
 import styles from "./Search.module.css"
 import backArrow from "./res/backArrow.svg"
 import { SearchPanel } from "../../components/SearchPanel/SearchPanel"
+import { Accordion } from "../../components/Accordion/Accordion"
 
 export const Search = () => {
     return (
@@ -12,6 +13,13 @@ export const Search = () => {
             <h1 className={styles.title}>Сервис патентного ландшафта</h1>
             <h2 className={styles.subTitle}>Поиск патентной документации</h2>
             <SearchPanel />
+            <div className={styles.favorites}>
+                <p className={styles.name}>Избранное</p>
+                <div className={styles.accordions}>
+                    <Accordion name={"Сохраненные поисковые запросы"}></Accordion>
+                    <Accordion name={"Избранные документы"}></Accordion>
+                </div>
+            </div>
         </div>
     )
 }
