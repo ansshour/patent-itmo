bb6e1ba3f90a6c28ca8c27d0d050456fea10da71
ansshour <andrey.zhukov001@gmail.com>
2022-12-17 17:50:09 +0300

    show img docs page

--- src/pages/SearchDetail/SearchDetail.jsx
+++ src/pages/SearchDetail/SearchDetail.jsx
@@ -21,11 +21,11 @@ export const SearchDetail = () => {
     const [data, setData] = useState(null)
     const [status, setStatus] = useState("loading")
     const [lang, setLang] = useState("ru")
+    const [referat, setReferat] = useState(null)
 
     const getPatent = async () => {
         try {
             const { data } = await fetchData(`/api/patents/docs/${id}`)
-            console.log(data)
             setData(data)
         } catch (err) {
 
@@ -33,6 +33,15 @@ export const SearchDetail = () => {
             setStatus("fullfiled")
         }
 
+    }
+
+    const setSrc = (data) => {
+        const node = document.createElement("div")
+        node.innerHTML = data;
+        node.querySelectorAll("img").forEach(node => {
+            node.setAttribute("src", `https://searchplatform.rospatent.gov.ru${node.getAttribute("src")}`)
+        })
+        return node.outerHTML;
     }
 
     useEffect(() => {
@@ -48,8 +57,14 @@ export const SearchDetail = () => {
                         <div className={styles.topInner}>
                             <Button blue>Похожие документы</Button>
                             <div className={styles.switchPage}>
-                                <img src={buttonWithArrow} className={styles.left} alt="left" />
-                                <img src={buttonWithArrow} alt="right" />
+                                <div className={styles.switchBtnWrapper}>
+                                    <div className={styles.disabled}></div>
+                                    <img src={buttonWithArrow} className={styles.left} alt="left" />
+                                </div>
+                                <div className={styles.switchBtnWrapper}>
+                                    <div className={styles.disabled} style={{ display: "none" }}></div>
+                                    <img src={buttonWithArrow} alt="right" />
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -83,82 +98,83 @@ export const SearchDetail = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={styles.content}>
-                        <p className={styles.contentTitle}>Библиография</p>
-                        <div className={styles.first}>
-                            <div className={styles.point}>
-                                <p className={styles.name}>Правовой статус:</p>
-                                <p className={styles.result}>отсутствует</p>
-                            </div>
-                            {data?.common.application.number && (
-                                <div className={styles.point}>
-                                    <p className={styles.name}>Номер заявки:</p>
-                                    <p className={styles.result}>{data?.common.application.number}</p>
+                    <div className={styles.closeContent}>
+                        <Accordion name={"Библиография"}>
+                            <div className={styles.content}>
+                                <div className={styles.first}>
+                                    <div className={styles.point}>
+                                        <p className={styles.name}>Правовой статус:</p>
+                                        <p className={styles.result}>отсутствует</p>
+                                    </div>
+                                    {data?.common.application.number && (
+                                        <div className={styles.point}>
+                                            <p className={styles.name}>Номер заявки:</p>
+                                            <p className={styles.result}>{data?.common.application.number}</p>
+                                        </div>
+                                    )}
+                                    {data?.common.application.filing_date && (
+                                        <div className={styles.point}>
+                                            <p className={styles.name}>Дата подачи заявки:</p>
+                                            <p className={styles.result}><p>{data?.common.application.filing_date}</p> <img src={searchIcon} className={styles.search} alt="search" /></p>
+                                        </div>
+                                    )}
+                                    {data?.common.publication_date && (
+                                        <div className={styles.point}>
+                                            <p className={styles.name}>Опубликовано:</p>
+                                            <p className={styles.result}><p>{data?.common.publication_date}</p> <img src={searchIcon} className={styles.search} alt="search" /></p>
+                                        </div>
+                                    )}
+                                    {data?.common.publishing_office && <p className={styles.result}>{data?.common.publishing_office} <span className={styles.blue}>{data?.common.document_number}</span> {data?.common.kind}</p>}
                                 </div>
-                            )}
-                            {data?.common.application.filing_date && (
-                                <div className={styles.point}>
-                                    <p className={styles.name}>Дата подачи заявки:</p>
-                                    <p className={styles.result}><p>{data?.common.application.filing_date}</p> <img src={searchIcon} className={styles.search} alt="search" /></p>
+                                <div className={styles.second}>
+                                    <div className={classNames(styles.point, styles.twoGrid)}>
+                                        <p className={styles.name}>Приоритетные данные:</p>
+                                        {data?.common.priority?.map(({ number, filing_date, publishing_office }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /><p>{`${number ? number : ""} ${filing_date ? filing_date : ""} ${publishing_office ? publishing_office : ""}`}</p></p>)}
+                                    </div>
+                                    {data?.common.classification.cpc && (
+                                        <div className={styles.point}>
+                                            <p className={styles.name}>СПК:</p>
+                                            {data?.common.classification.cpc?.map(({ fullname }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /> <p>{fullname}</p></p>)}
+                                        </div>
+                                    )}
+                                    {data?.common.classification.ipc && (
+                                        <div className={styles.point}>
+                                            <p className={styles.name}>МПК:</p>
+                                            {data?.common.classification.ipc?.map(({ fullname }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /> <p>{fullname}</p></p>)}
+                                        </div>
+                                    )}
+                                    <div></div>
                                 </div>
-                            )}
-                            {data?.common.publication_date && (
-                                <div className={styles.point}>
-                                    <p className={styles.name}>Опубликовано:</p>
-                                    <p className={styles.result}><p>{data?.common.publication_date}</p> <img src={searchIcon} className={styles.search} alt="search" /></p>
+                                <div className={styles.third}>
+                                    {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].applicant && (
+                                        <div className={classNames(styles.point, styles.twoGrid)}>
+                                            <p className={styles.name}>Заявители:</p>
+                                            {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].applicant?.map(({ name }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /><p>{name}</p></p>)}
+                                        </div>
+                                    )}
+                                    {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].inventor && (
+                                        <div className={classNames(styles.point, styles.fourColumns)}>
+                                            <p className={styles.name}>Авторы:</p>
+                                            <div className={styles.twoColumns}>
+                                                {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].inventor?.map(({ name }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /><p>{name}</p></p>)}
+                                            </div>
+                                        </div>
+                                    )}
                                 </div>
-                            )}
-                            {data?.common.publishing_office && <p className={styles.result}>{data?.common.publishing_office} <span className={styles.blue}>{data?.common.document_number}</span> {data?.common.kind}</p>}
-                        </div>
-                        <div className={styles.second}>
-                            <div className={classNames(styles.point, styles.twoGrid)}>
-                                <p className={styles.name}>Приоритетные данные:</p>
-                                {data?.common.priority?.map(({ number, filing_date, publishing_office }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /><p>{`${number ? number : ""} ${filing_date ? filing_date : ""} ${publishing_office ? publishing_office : ""}`}</p></p>)}
-                            </div>
-                            {data?.common.classification.cpc && (
-                                <div className={styles.point}>
-                                    <p className={styles.name}>СПК:</p>
-                                    {data?.common.classification.cpc?.map(({ fullname }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /> <p>{fullname}</p></p>)}
-                                </div>
-                            )}
-                            {data?.common.classification.ipc && (
-                                <div className={styles.point}>
-                                    <p className={styles.name}>МПК:</p>
-                                    {data?.common.classification.ipc?.map(({ fullname }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /> <p>{fullname}</p></p>)}
-                                </div>
-                            )}
-                            <div></div>
-                        </div>
-                        <div className={styles.third}>
-                            {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].applicant && (
-                                <div className={classNames(styles.point, styles.twoGrid)}>
-                                    <p className={styles.name}>Заявители:</p>
-                                    {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].applicant?.map(({ name }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /><p>{name}</p></p>)}
-                                </div>
-                            )}
-                            {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].inventor && (
-                                <div className={classNames(styles.point, styles.fourColumns)}>
-                                    <p className={styles.name}>Авторы:</p>
-                                    <div className={styles.twoColumns}>
-                                        {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].inventor?.map(({ name }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /><p>{name}</p></p>)}
+                                <div className={styles.fourth}>
+                                    {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].patentee && (
+                                        <div className={classNames(styles.point, styles.twoGrid)}>
+                                            <p className={styles.name}>Патентообладатели:</p>
+                                            {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].patentee?.map(({ name }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /><p>{name}</p></p>)}
+                                        </div>
+                                    )}
+                                    <div className={classNames(styles.point, styles.fourColumns)}>
+                                        <p className={styles.name}>Документы, цитированные в отчёте о поиске:</p>
+                                        {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].citations_parsed?.map(({ text }, i) => <p key={i} className={styles.result}>{text}</p>)}
                                     </div>
                                 </div>
-                            )}
-                        </div>
-                        <div className={styles.fourth}>
-                            {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].patentee && (
-                                <div className={classNames(styles.point, styles.twoGrid)}>
-                                    <p className={styles.name}>Патентообладатели:</p>
-                                    {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].patentee?.map(({ name }, i) => <p key={i} className={styles.result}><img src={searchIcon} className={styles.searchSecond} alt="search" /><p>{name}</p></p>)}
-                                </div>
-                            )}
-                            <div className={classNames(styles.point, styles.fourColumns)}>
-                                <p className={styles.name}>Документы, цитированные в отчёте о поиске:</p>
-                                {data?.biblio[(lang === "ru" && "ru") || (lang === "en" && "en")].citations_parsed?.map(({ text }, i) => <p key={i} className={styles.result}>{text}</p>)}
                             </div>
-                        </div>
-                    </div>
-                    <div className={styles.closeContent}>
+                        </Accordion>
                         {data?.drawings && (
                             <div className={styles.pictures}>
                                 <Accordion name={`Чертежи (${data?.drawings.length})`}>
@@ -171,21 +187,21 @@ export const SearchDetail = () => {
                         {data?.abstract[(lang === "ru" && "ru") || (lang === "en" && "en")] && (
                             <div className={styles.report}>
                                 <Accordion name="Реферат">
-                                    <p dangerouslySetInnerHTML={{ __html: data?.abstract[(lang === "ru" && "ru") || (lang === "en" && "en")] }} />
+                                    <p dangerouslySetInnerHTML={{ __html: setSrc(data?.abstract[(lang === "ru" && "ru") || (lang === "en" && "en")]) }} />
                                 </Accordion>
                             </div>
                         )}
                         {data?.claims[(lang === "ru" && "ru") || (lang === "en" && "en")] && (
                             <div className={styles.formula}>
                                 <Accordion name="Формула">
-                                    <div dangerouslySetInnerHTML={{ __html: data?.claims[(lang === "ru" && "ru") || (lang === "en" && "en")] }} />
+                                    <div dangerouslySetInnerHTML={{ __html: setSrc(data?.claims[(lang === "ru" && "ru") || (lang === "en" && "en")]) }} />
                                 </Accordion>
                             </div>
                         )}
                         {data?.description[(lang === "ru" && "ru") || (lang === "en" && "en")] && (
                             <div className={styles.description}>
                                 <Accordion name="Описание">
-                                    <p dangerouslySetInnerHTML={{ __html: data?.description[(lang === "ru" && "ru") || (lang === "en" && "en")] }} />
+                                    <p dangerouslySetInnerHTML={{ __html: setSrc(data?.description[(lang === "ru" && "ru") || (lang === "en" && "en")]) }} />
                                 </Accordion>
                             </div>
                         )}
